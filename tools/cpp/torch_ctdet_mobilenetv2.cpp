a6a7fc30c5a0a4230476916b922344cb57fe277d
Rong Jiang <835293711@qq.com>
2020-04-14 17:03:29 +0800

    Detector类

--- tools/cpp/torch_ctdet_mobilenetv2.cpp
+++ tools/cpp/torch_ctdet_mobilenetv2.cpp
@@ -1,3 +1,4 @@
+
 #include "core/Backend.hpp"
 #include "revertMNNModel.hpp"
 #include <MNN/Interpreter.hpp>
@@ -12,17 +13,10 @@
 
 #include <opencv2/opencv.hpp>
 
+#include "torch_ctdet_mobilenetv2.hpp"
+
 using namespace std;
 
-struct ObjInfo {
-	float x1;
-	float y1;
-	float x2;
-	float y2;
-	float score;
-	float area;
-	int label;
-};
 
 static inline uint64_t getTimeInUs() {
     uint64_t time;
@@ -41,9 +35,17 @@ static inline uint64_t getTimeInUs() {
     return time;
 }
 
-void _nms(std::vector<ObjInfo>& input, std::vector<ObjInfo>& output, float nmsthreshold,std::string type) {
+Detector::Detector()
+{
+}
+
+Detector::~Detector()
+{
+}
+
+int Detector::nms(std::vector<ObjInfo>& input, std::vector<ObjInfo>& output, float nmsthreshold, int type) {
 	if (input.empty()) {
-		return;
+		return 1;
 	}
 	std::sort(input.begin(), input.end(),
 		[](const ObjInfo& a, const ObjInfo& b)
@@ -78,9 +80,9 @@ void _nms(std::vector<ObjInfo>& input, std::vector<ObjInfo>& output, float nmsth
 			maxY = ((minY - maxY + 1) > 0) ? (minY - maxY + 1) : 0;
 			//IOU reuse for the area of two bbox
 			IOU = maxX * maxY;
-			if (type == "NMS_UNION")
+			if (type == NMS_UNION)
 				IOU = IOU / (input.at(it_idx).area + input.at(last).area - IOU);
-			else if (type == "NMS_MIN") {
+			else if (type == NMS_MIN) {
 				IOU = IOU / ((input.at(it_idx).area < input.at(last).area) ? input.at(it_idx).area : input.at(last).area);
 			}
 			if (IOU > nmsthreshold) {
@@ -96,109 +98,47 @@ void _nms(std::vector<ObjInfo>& input, std::vector<ObjInfo>& output, float nmsth
 	for (int i = 0; i < nPick; i++) {
 		output[i] = input[vPick[i]];
 	}
+    return 0;
 }
 
-int main(int argc, const char* argv[])
+int Detector::init(std::string model_path)
 {
-    if (argc != 3) {
-        // MNN_PRINT("Usage: ./torch_ctdet_mobilenetv2.out /workspace/centernet/models/pascal_mobilenetv2_384.mnn /workspace/centernet/models/2_origin_pred_1.0.jpg\n");
-        MNN_PRINT("Usage: ./torch_ctdet_mobilenetv2.out /workspace/centernet/models/pascal_mobilenetv2_384_sigmoid.mnn /workspace/centernet/models/StereoVision_L_803031_-10_0_0_6821_D_Shoe_714_-1080_Shoe_659_-971.jpeg\n");
-        return 0;
-    }
-    std::string image_name = argv[2];
-    std::string model_name = argv[1];
-
-    /* HYPERPARAMETER */
-    const int forward = MNN_FORWARD_OPENCL;
-    const int precision = 2;
-    const int power     = 0;
-    const int memory    = 0;
-    const int threads   = 1;
-    const int INPUT_SIZE = 384;
-    const float scale = 4.0f;
-    const int C = 27;
-    const int H = INPUT_SIZE / scale;
-    const int W = INPUT_SIZE / scale;
-    const int inputImageHeight = 960;
-    const int inputImageWidth = 1280;
-    const float scoreThreshold = 0.1f;
-    const float iouThreshold = 0.5f;
-    const cv::Mat transInput = (cv::Mat_<float>(2,3)<<0.3f,-0.f,0.f,0.f,0.3f,48.f);
-    const cv::Mat mean(INPUT_SIZE, INPUT_SIZE, CV_32FC3, cv::Scalar(0.408f, 0.447f, 0.47f));
-    const cv::Mat std(INPUT_SIZE, INPUT_SIZE, CV_32FC3, cv::Scalar(0.289f, 0.274f, 0.278f));
-    const std::string visImg = "./torch_ctdet_mobilenetv2_result.jpg";
-    const char * whTensorID = "681";
-    const char * hmTensorID = "677";
-    const char * regTensorID = "685";
-
-    /* LOAD MODEL */
-    auto revertor = std::unique_ptr<Revert>(new Revert(model_name.c_str()));
+    auto revertor = std::unique_ptr<Revert>(new Revert(model_path.c_str()));
     revertor->initialize();
     auto modelBuffer      = revertor->getBuffer();
     const auto bufferSize = revertor->getBufferSize();
-    auto net = std::shared_ptr<MNN::Interpreter>(MNN::Interpreter::createFromBuffer(modelBuffer, bufferSize));
+    net = std::shared_ptr<MNN::Interpreter>(MNN::Interpreter::createFromBuffer(modelBuffer, bufferSize));
     revertor.reset();
     MNN::ScheduleConfig config;
     config.numThread = threads;
     config.type      = static_cast<MNNForwardType>(forward);
     MNN::BackendConfig backendConfig;
-    backendConfig.precision = (MNN::BackendConfig::PrecisionMode)precision;
+    backendConfig.precision = (MNN::BackendConfig::PrecisionMode) precision;
     backendConfig.power = (MNN::BackendConfig::PowerMode) power;
     backendConfig.memory = (MNN::BackendConfig::MemoryMode) memory;
     config.backendConfig = &backendConfig;
-    auto session = net->createSession(config);
+    session = net->createSession(config);
     net->releaseModel();
-    
-    /* PRE-PROCESS */
-    auto tic = getTimeInUs();
-    cv::Mat raw_image    = cv::imread(image_name.c_str());
-    cv::Size raw_imageSize =  raw_image.size();
-    MNN_CHECK(raw_imageSize.height == inputImageHeight, "input image height error");
-    MNN_CHECK(raw_imageSize.width == inputImageWidth, "input image width error");
-    cv::Mat image;
-    cv::Mat affinedImage;
-    cv::warpAffine(raw_image, affinedImage, transInput, cv::Size(INPUT_SIZE, INPUT_SIZE), cv::INTER_LINEAR);
-    affinedImage.convertTo(image, CV_32FC3);
-    image = (image / 255.0f - mean) / std;
+    nhwc_Tensor = MNN::Tensor::create<float>({1, INPUT_SIZE, INPUT_SIZE, 3}, NULL, MNN::Tensor::TENSORFLOW);
+    wh  = net->getSessionOutput(session, whTensorID);
+    hm_sigmoid  = net->getSessionOutput(session, hmTensorID);
+    reg  = net->getSessionOutput(session, regTensorID);
+    return 0;
+}
 
-    // wrapping input tensor, convert nhwc to nchw    
-    std::vector<int> dims{1, INPUT_SIZE, INPUT_SIZE, 3};
-    auto nhwc_Tensor = MNN::Tensor::create<float>(dims, NULL, MNN::Tensor::TENSORFLOW);
-    auto nhwc_data   = nhwc_Tensor->host<float>();
-    auto nhwc_size   = nhwc_Tensor->size();
-    ::memcpy(nhwc_data, image.data, nhwc_size);
-    auto toc = getTimeInUs();
-    printf("pre-precess costs: %8.3fms\n", (toc - tic) / 1000.0f);
-
-    /* INFERENCE */
-    tic = getTimeInUs();
-    auto inputTensor  = net->getSessionInput(session, nullptr);
-    inputTensor->copyFromHostTensor(nhwc_Tensor);
-    net->runSession(session);
-
-    
-    MNN::Tensor *wh  = net->getSessionOutput(session, whTensorID);
+int Detector::decode(std::vector<ObjInfo>& objs_tmp) {
     MNN::Tensor wh_host(wh, wh->getDimensionType());
     wh->copyToHostTensor(&wh_host);
     auto wh_dataPtr  = wh_host.host<float>();
 
-    MNN::Tensor *hm_sigmoid  = net->getSessionOutput(session, hmTensorID);
     MNN::Tensor hm_host(hm_sigmoid, hm_sigmoid->getDimensionType());
     hm_sigmoid->copyToHostTensor(&hm_host);
     auto hm_dataPtr  = hm_host.host<float>();
 
-    MNN::Tensor *reg  = net->getSessionOutput(session, regTensorID);
     MNN::Tensor reg_host(reg, reg->getDimensionType());
     reg->copyToHostTensor(&reg_host);
     auto reg_dataPtr  = reg_host.host<float>();
-
-    toc = getTimeInUs();
-    printf("inference costs: %8.3fms\n", (toc - tic) / 1000.0f);
-
-    /* POST-PRECESS */
-    tic = getTimeInUs();
-    // decode
-    std::vector<ObjInfo> objs_tmp;
+    
     int idx = 0;
     for (int c = 0; c < C; c++){
         for (int h = 0; h < H; h++) {
@@ -219,7 +159,7 @@ int main(int argc, const char* argv[])
                     objbox.y1 = ((centerY + yReg) - height / 2.0f) * scale;
                     objbox.x2 = ((centerX + xReg) + width / 2.0f) * scale;
                     objbox.y2 = ((centerY + yReg) + height / 2.0f) * scale;
-                    std::cout << "score: " << objbox.score << ", label: " << objbox.label << ", x1: " << objbox.x1 << ", x2: " << objbox.x2 << ", y1: " << objbox.y1 << ", y2: " << objbox.y2 << std::endl;
+                    // std::cout << "score: " << objbox.score << ", label: " << objbox.label << ", x1: " << objbox.x1 << ", x2: " << objbox.x2 << ", y1: " << objbox.y1 << ", y2: " << objbox.y2 << std::endl;
                     objbox.area=(objbox.x2 - objbox.x1) * (objbox.y2 - objbox.y1);
                     objs_tmp.push_back(objbox);
                 }
@@ -227,12 +167,57 @@ int main(int argc, const char* argv[])
             }
         }
     }
-    // NMS
-    std::vector<ObjInfo> objs;
-    _nms(objs_tmp, objs, iouThreshold, "NMS_UNION");
+    return 0;
+}
 
+int Detector::preProcess(std::string image_path) {
+    /* PRE-PROCESS */
+    const cv::Mat mean(INPUT_SIZE, INPUT_SIZE, CV_32FC3, meanValue);
+    const cv::Mat std(INPUT_SIZE, INPUT_SIZE, CV_32FC3, stdValue);
+    cv::Mat raw_image    = cv::imread(image_path);
+    cv::Size raw_imageSize =  raw_image.size();
+    MNN_CHECK(raw_imageSize.height == inputImageHeight, "input image height error");
+    MNN_CHECK(raw_imageSize.width == inputImageWidth, "input image width error");
+    cv::warpAffine(raw_image, affinedImage, transInput, cv::Size(INPUT_SIZE, INPUT_SIZE), cv::INTER_LINEAR);
+    cv::Mat image;
+    affinedImage.convertTo(image, CV_32FC3);
+    image = (image / 255.0f - mean) / std;
+    ::memcpy(nhwc_Tensor->host<float>(), image.data, nhwc_Tensor->size());
+    return 0;
+}
+
+int Detector::inference() {
+    auto inputTensor  = net->getSessionInput(session, nullptr);
+    inputTensor->copyFromHostTensor(nhwc_Tensor);
+    net->runSession(session);
+    return 0;
+}
+
+int Detector::detect(std::string image_path) {
+    /* PRE-PROCESS */
+    auto tic = getTimeInUs();
+    preProcess(image_path);
+    auto toc = getTimeInUs();
+    printf("pre-precess costs: %8.3fms\n", (toc - tic) / 1000.0f);
+
+    /* INFERENCE */
+    tic = getTimeInUs();
+    inference();
     toc = getTimeInUs();
-    printf("post-precess costs: %8.3fms\n", (toc - tic) / 1000.0f);
+    printf("runSession costs: %8.3fms\n", (toc - tic) / 1000.0f);
+
+    /* POST-PRECESS */
+    tic = getTimeInUs();
+    std::vector<ObjInfo> objs_tmp;
+    decode(objs_tmp);
+    toc = getTimeInUs();
+    printf("decode costs: %8.3fms\n", (toc - tic) / 1000.0f);
+
+    tic = getTimeInUs();
+    std::vector<ObjInfo> objs;
+    nms(objs_tmp, objs, iouThreshold, NMS_UNION);
+    toc = getTimeInUs();
+    printf("nms costs: %8.3fms\n", (toc - tic) / 1000.0f);
 
     /* VISUALIZATION */
     for (auto obj: objs) {
@@ -244,5 +229,23 @@ int main(int argc, const char* argv[])
         cv::rectangle(affinedImage, vis_box, cv::Scalar(0,0,255), 2);
     }
     cv::imwrite(visImg, affinedImage);
+
+    return 0;
+}
+
+
+int main(int argc, const char* argv[])
+{
+    if (argc != 3) {
+        // MNN_PRINT("Usage: ./torch_ctdet_mobilenetv2.out /workspace/centernet/models/pascal_mobilenetv2_384.mnn /workspace/centernet/models/2_origin_pred_1.0.jpg\n");
+        MNN_PRINT("Usage: ./torch_ctdet_mobilenetv2.out /workspace/centernet/models/pascal_mobilenetv2_384_sigmoid.mnn /workspace/centernet/models/StereoVision_L_803031_-10_0_0_6821_D_Shoe_714_-1080_Shoe_659_-971.jpeg\n");
+        return 0;
+    }
+    std::string image_name = argv[2];
+    std::string model_name = argv[1];
+    Detector detector;
+    detector.init(model_name);
+    detector.detect(image_name);
+
 }
 
---
+++ tools/cpp/torch_ctdet_mobilenetv2.hpp
@@ -0,0 +1,64 @@
+#include <opencv2/opencv.hpp>
+#include <MNN/Interpreter.hpp>
+
+#define NMS_UNION 1
+#define NMS_MIN  2
+
+
+struct ObjInfo {
+	float x1;
+	float y1;
+	float x2;
+	float y2;
+	float score;
+	float area;
+	int label;
+};
+
+class Detector {
+public:
+	Detector();
+	~Detector();
+	/* HYPERPARAMETER */
+    const int forward = MNN_FORWARD_OPENCL;
+	// const int forward = MNN_FORWARD_CPU;
+    const int precision = 2;
+    const int power     = 0;
+    const int memory    = 0;
+    const int threads   = 4;
+    const int INPUT_SIZE = 384;
+    const char * whTensorID = "681";
+    const char * hmTensorID = "677";
+    const char * regTensorID = "685";
+    const float scale = 4.0f;
+    const int C = 27;
+    const int H = INPUT_SIZE / scale;
+    const int W = INPUT_SIZE / scale;
+    const float scoreThreshold = 0.1f;
+    const int inputImageHeight = 960;
+    const int inputImageWidth = 1280;
+    const float iouThreshold = 0.5f;
+    const cv::Mat transInput = (cv::Mat_<float>(2,3)<<0.3f,-0.f,0.f,0.f,0.3f,48.f);
+    const std::string visImg = "./torch_ctdet_mobilenetv2_result.jpg";
+	const cv::Scalar meanValue = cv::Scalar(0.408f, 0.447f, 0.47f);
+	const cv::Scalar stdValue = cv::Scalar(0.289f, 0.274f, 0.278f);
+
+	int init(std::string model_path);
+	int preProcess(std::string image_path);
+	int detect(std::string image_path);
+	int inference();
+
+private: 
+	std::shared_ptr<MNN::Interpreter> net;
+	MNN::Session * session;
+    MNN::Tensor * nhwc_Tensor;
+	MNN::Tensor * wh;
+	MNN::Tensor * hm_sigmoid;
+	MNN::Tensor * reg;
+	cv::Mat affinedImage;
+
+	int decode(std::vector<ObjInfo>& objs_tmp);
+	// borrow from: https://github.com/ouyanghuiyu/centernet_mobilenetv2_ncnn/blob/master/cpp/ncnn_centernet.cpp
+	int nms(std::vector<ObjInfo>& input, std::vector<ObjInfo>& output, float nmsthreshold, int type);
+
+};
